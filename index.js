171c3cc0f046d252e7283e9651cb8d4e0b1a3247
JasonLinn <cheng11220@gmail.com>
2017-07-02 22:06:02 +0800

    ha

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-
+prot = process.env.PORT || 5000;
 
 app.use('/',function(req,res) {
     res.send(`<html><head>
         </head><body>Hello</body></html>`)
 })
-app.listen(3001)
+app.listen(prot);
